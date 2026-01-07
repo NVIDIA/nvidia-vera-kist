2000c22f47cbb464a23fecf7ab886bae25445aca
Nate Watterson <nwatterson@nvidia.com>
2026-01-07 14:23:56 +0000

    itm: Cleanup unused files
    JIRA IST-2626

--- include/template_app.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstdint>
-#include <string>
-
-int changeIntegerValue(const int& requestedValue, int& currentValue);
-
-std::string unlockDoor(const std::string& requestedVal);
